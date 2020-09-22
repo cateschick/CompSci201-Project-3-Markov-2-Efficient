6c543f1fe5113b0aacf5ff8a67377812c40aa621
Cate Schick <catherine.schick@duke.edu>
2020-09-22 16:37:42 -0400

    finished coding for p3

--- src/EfficientMarkov.java
+++ src/EfficientMarkov.java
@@ -1,14 +1,59 @@
 import java.util.*;
 
 public class EfficientMarkov extends BaseMarkov {
-	private Map<String,ArrayList<String>> myMap;
-	
-	EfficientMarkov(){
+	private Map<String, ArrayList<String>> myMap;
+
+	EfficientMarkov() {
 		this(3);
+		myMap = new HashMap<String, ArrayList<String>>();
 	}
 
 	EfficientMarkov(int order) {
 		super(order);
 		myMap = new HashMap<>();
 	}
-}	
+
+	@Override
+	public void setTraining(String text) {
+		// set myText to parameter text
+		myText = text;
+
+		// clear the map
+		myMap.clear();
+
+		// initialize the map
+
+		// string
+		String x;
+		// next string
+		String y;
+
+		for (int i = 0; i < myText.length() + 1 - myOrder; i++) {
+
+			// create key
+			x = myText.substring(i, i + myOrder);
+
+			// store keys
+			if (!myMap.containsKey(x)) {
+				myMap.put(x, new ArrayList<String>());
+			}
+			if (myText.length() == i + myOrder) {
+				myMap.get(x).add(PSEUDO_EOS);
+			} else {
+				y = myText.substring(myOrder + i, myOrder + i + 1);
+				myMap.get(x).add(y);
+			}
+		}
+	}
+
+	@Override
+	public ArrayList<String> getFollows(String key) {
+		// look up the key in the map
+		if (!myMap.containsKey(key)) {
+			// the key is not in the map
+			throw new NoSuchElementException(key + " not in map");
+		}
+		// return associated value
+			return myMap.get(key);
+	}
+}
--- src/EfficientWordMarkov.java
+++ src/EfficientWordMarkov.java
@@ -12,6 +12,7 @@ public class EfficientWordMarkov extends BaseWordMarkov {
         this(3);
     }
 
+    // getFollows is the same as in EfficientMarkov, except myMap is different
     @Override
     public ArrayList<String> getFollows(WordGram key) {
         if (!myMap.containsKey(key)) {
@@ -22,12 +23,21 @@ public class EfficientWordMarkov extends BaseWordMarkov {
 
     @Override
     public void setTraining(String text) {
+        // create an array of words
         myWords = text.split("\\s+");
+
+        // clear the map
         myMap.clear();
+
+        // same general idea as Efficient Markov
         for (int i = 0; i < myWords.length - myOrder; i++) {
+
+            // create a new wordgram instead of substring
             WordGram word = new WordGram(myWords, i, myOrder);
+
             myMap.putIfAbsent(word, new ArrayList<String>());
             myMap.get(word).add(myWords[i + myOrder]);
+
         }
         WordGram lastWord = new WordGram(myWords, myWords.length - myOrder, myOrder);
         myMap.putIfAbsent(lastWord, new ArrayList<String>());
